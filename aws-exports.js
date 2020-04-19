28342ac214c91e0f4d46aef72770cd266241265d
Makoto Ishida <makotoishida@gmail.com>
2020-04-18 17:16:02 -1000

    Add API, Function and DB.

--- aws-exports.js
+++ aws-exports.js
@@ -2,10 +2,25 @@
 
 const awsmobile = {
     "aws_project_region": "us-west-2",
+    "aws_cognito_identity_pool_id": "us-west-2:39ac27d3-10c3-4609-913e-f01bf8e573c9",
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_HaojCQ3jz",
     "aws_user_pools_web_client_id": "21rl8ose5199100nja1lsrotk7",
-    "oauth": {}
+    "oauth": {},
+    "aws_dynamodb_all_tables_region": "us-west-2",
+    "aws_dynamodb_table_schemas": [
+        {
+            "tableName": "todo-dev",
+            "region": "us-west-2"
+        }
+    ],
+    "aws_cloud_logic_custom": [
+        {
+            "name": "amplifytest01api",
+            "endpoint": "https://de62hdrc0j.execute-api.us-west-2.amazonaws.com/dev",
+            "region": "us-west-2"
+        }
+    ]
 };
 
 
